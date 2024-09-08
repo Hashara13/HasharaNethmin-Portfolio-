2cda8e7e085a40e719f9b3aa42db52d840ef489a
Hashara13 <hnethmin13@gmail.com>
2024-09-08 10:16:53 +0530

    minor changes

--- form.php
+++ form.php
@@ -1,26 +1,38 @@
-
-
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; 
+
+$config = include('config.php'); 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $Name = $_POST['Name'];
     $Email = $_POST['Email'];
     $Message = $_POST['Message'];
 
-    $conn = new mysqli('localhost', 'root', '', 'test1');
-    if ($conn->connect_error) {
-        die('Connection failed: ' . $conn->connect_error);
-    } else {
-        $stmt = $conn->prepare("INSERT INTO form (Name, Email, Message) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $Name, $Email, $Message);
+    $mail = new PHPMailer(true);
 
-        if ($stmt->execute()) {
-            echo "Thank You!";
-        } else {
-            echo "Error: " . $stmt->error;
-        }
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com'; 
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $config['gmail_username'];
+        $mail->Password   = $config['gmail_password']; 
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
 
-        $stmt->close();
-        $conn->close();
+        $mail->setFrom($Email, $Name);
+        $mail->addAddress('nethhashara@gmail.com');
+
+        $mail->isHTML(true); 
+        $mail->Subject = 'New message from Contact Form';
+        $mail->Body    = "<strong>Name:</strong> $Name<br><strong>Email:</strong> $Email<br><strong>Message:</strong><br>$Message";
+        $mail->AltBody = "Name: $Name\nEmail: $Email\nMessage: $Message";
+
+        $mail->send();
+        echo "Message has been sent";
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
-?>
